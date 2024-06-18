03408df458643ea6c015b7f965d256321a5c2dde
vinodkyerra <vinodkyerra@gmail.com>
2024-06-18 10:39:36 +0530

    variable

--- 01-variable.sh
+++ 01-variable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-This command $() will executed and stored the value in variable called DATE
-DATE=$()
+#shell script run command $() and stored the value in variable called DATE
+DATE=$(date)
 
-echo"This script is executed at timestamp:$DATE"
+echo"This script is executed at timestamp: $DATE "
