c4b14182fd1a18f2fc3bc7cc73748642f406af0b
vinodkyerra <vinodkyerra@gmail.com>
2024-06-18 09:58:03 +0530

    content added

--- shell.sh
+++ shell.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
  echo " we are learning aws with devops"
  echo "The concept of variables come into picture with principle called DRY dont repeat yourslef"
- 
  PERSON1=Vinod
  PERSON2=lucky
 
