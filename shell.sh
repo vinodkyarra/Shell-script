d55b70fdef710b04cf0302a0fe1e801887e91ea2
vinodkyerra <vinodkyerra@gmail.com>
2024-06-18 09:56:28 +0530

    content added

--- shell.sh
+++ shell.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
  echo " we are learning aws with devops"
- echo "The concept of variables come into picture with principle called DRY dont repeat yourslef "
+ echo "The concept of variables come into picture with principle called DRY dont repeat yourslef"
+ 
+ PERSON1=Vinod
+ PERSON2=lucky
+
+ echo "$PERSON1:how are you $PERSON2"
+ echo "$PERSON2: I good, how are you $PERSON1"
+ echo "$PERSON1:I am well, whare are you $PERSON2"
+ echo "$PERSON2: i am at my work palce $PERSON1"
