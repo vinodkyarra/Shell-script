34cd94e4e6695ab399f8d89d9099b1fe87c525aa
vinodkyerra <vinodkyerra@gmail.com>
2024-06-18 11:09:29 +0530

    entering username and pwd through command line

---
+++ 04-variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Please enter your username"
+
+read -s USERNAME
+
+echo "Enter your username:$USERNAME"
+
+echo "Please enter your password"
+
+read -s PASSWORD
+
+echo "Enter your username:$PASSWORD"
