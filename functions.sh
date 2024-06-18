d8d01dfffa7adc8dfa9a7366d3453dca7101a9b7
vinodkyerra <vinodkyerra@gmail.com>
2024-06-18 17:13:23 +0530

    Functions

---
+++ functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+   then 
+       echo $2 "error"
+       exit 1
+    else 
+       echo $2 "installtion is success"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root privilages"
+    exit 1
+# else 
+#     echo "  run this script"
+
+fi
+ 
+   yum install mysql -y
+
+VALIDATE $? "Mysql is success"
+
+   yum install postfix -y
+
+VALIDATE $? "Postfix is success"     
+
